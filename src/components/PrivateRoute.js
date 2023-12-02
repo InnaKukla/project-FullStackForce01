2da921f79e5a48c8d037e8ec9e2d3f54eb157308
Inna Kukla <0969125133@ukr.net>
2023-12-03 01:53:54 +0200

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,14 +1,17 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import authSelector from 'redux/auth/authSelector';
+import Loader from './Loader/Loader';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const isRefreshing = useSelector(authSelector.loadingSelector);
-  const isLogged = useSelector(authSelector.loggedInSelector);
-
-  const shouldRedirect = !isLogged && !isRefreshing;
-
-  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+  const { isLoggedIn, token } = useSelector(authSelector.selectAuth);
+  if (!isLoggedIn && token) {
+    return <Loader />;
+  }
+  if (!isLoggedIn && !token) {
+    return <Navigate to={redirectTo} />;
+  }
+  return Component;
 };
 
 export default PrivateRoute;
